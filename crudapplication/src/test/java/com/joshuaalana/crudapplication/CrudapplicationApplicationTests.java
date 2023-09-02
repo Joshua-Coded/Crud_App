989d3b793ace75356f93383aa5af0324ea03a1b6
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-02 12:42:40 +0200

    started it on the console

---
+++ crudapplication/src/test/java/com/joshuaalana/crudapplication/CrudapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshuaalana.crudapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
